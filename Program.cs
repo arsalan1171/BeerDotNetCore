82dd7f098945059f27997e0821fdfff2ac263c8d
arsalan665@gmail.com <arsalan665@gmail.com>
2023-06-17 17:21:04 +0200

    remove the if statement to enable swagger on azure

--- Program.cs
+++ Program.cs
@@ -16,11 +16,10 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors(builder =>
     {
